33ff608a5f56519ae8f35df9a334a3edfda272e0
Orbital Oyster <orbitaloyster@gmail.com>
2025-08-26 23:01:22 +0300

    Added yaml-cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 #include <string>
+#include <yaml-cpp/yaml.h>
 
 int main(int argc, char *argv[]) {
-  std::string s = "Hello, World!";
-  std::cout << s << std::endl;
+  std::string filename = "config.yaml";
+  YAML::Node config = YAML::LoadFile(filename);
+  YAML::Node window = config["window"];
+  std::string title = window["title"].as<std::string>();
+  int window_width = window["width"].as<int>();
+  int window_height = window["height"].as<int>();
+
+  std::cout << title << std::endl;
+  std::cout << window_width << std::endl;
+  std::cout << window_height << std::endl;
 }
