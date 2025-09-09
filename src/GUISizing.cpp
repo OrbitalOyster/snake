0bf9c1ffc3dc0c68c80d0226db2087ff66bcb4f4
OrbitalOyster <orbitaloyster@gmail.com>
2025-09-09 19:09:26 +0300

    Progress

--- src/GUISizing.cpp
+++ src/GUISizing.cpp
@@ -3,9 +3,9 @@
 #include <stdexcept>
 
 GUISizing::GUISizing() {
-    u1 = GUIUnit(0u);
-    u2 = GUIUnit();
-    u3 = GUIUnit(0u);
+  u1 = GUIUnit(0u);
+  u2 = GUIUnit();
+  u3 = GUIUnit(0u);
 }
 
 GUISizing::GUISizing(GUIUnit u1, GUIUnit u2, GUIUnit u3)
@@ -15,22 +15,18 @@ GUISizing::GUISizing(GUIUnit u1, GUIUnit u2, GUIUnit u3)
 }
 
 void GUISizing::calculate(float root_size, float *l, float *s) {
-    int u1p = 0, u2p = 0, u3p = 0;
+  int u1p = 0, u2p = 0;
 
-    if (!u1.is_computable())
-      u1p = u1.to_pixels(root_size);
-    if (!u2.is_computable())
-      u2p = u2.to_pixels(root_size);
-    if (!u3.is_computable())
-      u3p = u3.to_pixels(root_size);
+  if (u1.is_computable())
+    u1p = root_size - u2.to_pixels(root_size) - u3.to_pixels(root_size);
+  else
+    u1p = u1.to_pixels(root_size);
 
-    if (u1.is_computable())
-      u1p = root_size - u2p - u3p;
-    if (u2.is_computable())
-      u2p = root_size - u1p - u3p;
-    if (u3.is_computable())
-      u3p = root_size - u1p - u2p;
+  if (u2.is_computable())
+    u2p = root_size - u1.to_pixels(root_size) - u3.to_pixels(root_size);
+  else
+    u2p = u2.to_pixels(root_size);
 
-    *l = u1p;
-    *s = u2p;
+  *l = u1p;
+  *s = u2p;
 }
